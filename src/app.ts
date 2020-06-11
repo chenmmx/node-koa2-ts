2adc868994442f1cf37e2bfd5a1346f93d5ecef9
chenmmx <1107204891@qq.com>
2020-06-11 17:27:16 +0800

    fix: tslint error

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,8 @@ import { autoRouter } from 'koa-route-decors';
 import * as bodyparser from 'koa-bodyparser';
 import * as compress from 'koa-compress';
 import { errorHander } from './middleware/error.middleware';
-import{responseHandle} from './middleware/resoponse.middleware'
-import {jwt} from './middleware/jwt.middleware'
+import{ responseHandle } from './middleware/resoponse.middleware';
+import { jwt } from './middleware/jwt.middleware';
 import { connection } from './database/index';
 
 // tslint:disable-next-line: no-floating-promises
@@ -16,7 +16,7 @@ async function start () {
   const app = new Koa();
   const router = new Router();
   const subRouter = await autoRouter(path.resolve(__dirname, './'));
-  router.use(subRouter.routes(),jwt) //jwt验证路由
+  router.use(subRouter.routes(), jwt); // jwt验证路由
   app.use(errorHander)
   .use(bodyparser())
   .use(compress())
